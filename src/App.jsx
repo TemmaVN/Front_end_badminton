bd7ca241de2c9e4661ef3f77b90ef235ce4ccd9c
QMinh-2005 <quanminh88098@gmail.com>
2026-04-23 14:38:18 +0700

    Minh's Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import PageHeader from './layouts/PageHeader'
-import MainHeader from './layouts/MainHeader'
-import {useMediaQuery} from './mystate/useMediaQuery'
-import Login from './layouts/Login';
-import Register from './layouts/Register';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Advertisement from './components/Advertisement';
-import Contract from './layouts/Contract';
-import Sales from './layouts/Sales';
-
+import PageHeader from "./layouts/PageHeader";
+import MainHeader from "./layouts/MainHeader";
+import { useMediaQuery } from "./mystate/useMediaQuery";
+import Login from "./layouts/Login";
+import Register from "./layouts/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Advertisement from "./components/Advertisement";
+import Contract from "./layouts/Contract";
+import Sales from "./layouts/Sales";
+import ProductFrame_Minh from "./components/ProductFrame_Minh";
+import racket from "./assets/victor-auraspeed-lyc-2025-01a-150x150.webp";
 function App() {
-  const isHideMainHeader = useMediaQuery('(min-width: 1250px)');
+  const isHideMainHeader = useMediaQuery("(min-width: 1250px)");
   return (
     <BrowserRouter>
-      <div className='bg-white h-auto w-full'>
-        <PageHeader/>
-          {isHideMainHeader && <MainHeader></MainHeader>}
+      <div className="bg-white h-auto w-full">
+        <PageHeader />
+        {isHideMainHeader && <MainHeader></MainHeader>}
         <Routes>
-          <Route path='/' element={<Advertisement/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/contract' element={<Contract/>}/>
-          <Route path='/sales' element={<Sales/>}/>
+          <Route path="/" element={<Advertisement />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contract" element={<Contract />} />
+          <Route path="/sales" element={<Sales />} />
         </Routes>
-      </div> 
+        {/* Lấy dữ liệu từ BE đưa vào */}
+        <ProductFrame_Minh
+          image={racket}
+          productName={
+            "Vợt Cầu Lông Victor Auraspeed LYC 2025 (Liu Yu Chen) Limited"
+          }
+          basePrice={"100000"}
+          sellingPrice={"90000"}
+          isBestSeller={true}
+          discountPercent="20%"
+        ></ProductFrame_Minh>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import Button from './Button'
-import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
+import React, { useEffect, useState } from "react";
+import Button from "./Button";
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
 
 const Advertisement = () => {
   const linkAdvertisement = [
@@ -8,53 +8,58 @@ const Advertisement = () => {
     "https://static.fbshop.vn/wp-content/uploads/2025/12/he-thong-cau-long.png",
     "https://static.fbshop.vn/wp-content/uploads/2024/01/Banner-website-4-min.webp",
     "https://static.fbshop.vn/wp-content/uploads/2024/01/Banner-website-6-min.webp",
-    "https://static.fbshop.vn/wp-content/uploads/2026/01/anh-banner-website-4000x1425-1-1920x684.jpg"
+    "https://static.fbshop.vn/wp-content/uploads/2026/01/anh-banner-website-4000x1425-1-1920x684.jpg",
   ];
-  const [countAd,setCountAd] = useState(0);
+  const [countAd, setCountAd] = useState(0);
   const length = linkAdvertisement.length;
   useEffect(() => {
     const slideTimer = setInterval(() => {
-      setCountAd((prev) => (prev+1)%length);
+      setCountAd((prev) => (prev + 1) % length);
     }, 3000);
 
     return () => clearInterval(slideTimer);
-  }, [length])
+  }, [length]);
   return (
-    <div className='w-full overflow-hidden relative group'>
-        <div 
-        className={`flex ${countAd == 0? '':'transition-transform duration-700'} ease-in-out`}
-        style={{ transform: `translateX(-${countAd *100}%)`}}
+    <div className="w-full overflow-hidden relative group">
+      <div
+        className={`flex ${countAd == 0 ? "" : "transition-transform duration-700"} ease-in-out`}
+        style={{ transform: `translateX(-${countAd * 100}%)` }}
+      >
+        {linkAdvertisement.map((url, index) => (
+          <img
+            key={index}
+            src={url}
+            alt={`ad ${index}`}
+            className="w-full shrink-0 object-cover "
+          />
+        ))}
+      </div>
+      <div className="absolute inset-0 flex justify-between items-center px-4 opacity-0 group-hover:opacity-100 transition-opacity">
+        <Button
+          size="icon"
+          className="text-orange-dark hover:bg-orange-400"
+          onClick={() => setCountAd((countAd - 1 + length) % length)}
         >
-          {linkAdvertisement.map((url, index) => (
-            <img
-              key={index}
-              src={url}
-              alt={`ad ${index}`}
-              className='w-full shrink-0 object-cover '
-            />
-          ))}
-        </div>
-        <div className='absolute inset-0 flex justify-between items-center px-4 opacity-0 group-hover:opacity-100 transition-opacity'>
-          <Button 
-          size='icon' 
-          className='text-orange-dark hover:bg-orange-400'
-          onClick={() => setCountAd((countAd-1+length)%length)}
-          ><ArrowBigLeft/></Button>
-          <Button 
-          size='icon' 
-          className='text-orange-dark hover:bg-orange-400'
-          onClick={() => setCountAd((countAd+1)%length)}
-          ><ArrowBigRight/></Button>
-        </div>
-        <div className='absolute bottom-4 left-0 w-full flex justify-center gap-3 z-20'>
-          {
-            linkAdvertisement.map((items,index) => (
-              <div key={index} className={`w-3 h-3 ${index == (countAd % length)? 'bg-blue-400': 'bg-white'} rounded-full`}></div>
-            ))
-          }
-        </div>
+          <ArrowBigLeft />
+        </Button>
+        <Button
+          size="icon"
+          className="text-orange-dark hover:bg-orange-400"
+          onClick={() => setCountAd((countAd + 1) % length)}
+        >
+          <ArrowBigRight />
+        </Button>
+      </div>
+      <div className="absolute bottom-4 left-0 w-full flex justify-center gap-3 z-20">
+        {linkAdvertisement.map((items, index) => (
+          <div
+            key={index}
+            className={`w-3 h-3 ${index == countAd % length ? "bg-blue-400" : "bg-white"} rounded-full`}
+          ></div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Advertisement;
